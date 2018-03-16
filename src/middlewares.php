bbc4ab4edb5372d4011ee4f64efc3017bd1dd7d3
Clement Barbaza <clement.barbaza@gmail.com>
2018-03-16 18:27:59 +0100

    add client ip address middleware

--- src/middlewares.php
+++ src/middlewares.php
@@ -10,3 +10,10 @@ $app->add(new \Slim\Middleware\HttpBasicAuthentication([
     ]
 ]));
 */
+
+// Client IP address
+/*
+$checkProxyHeaders = true;
+$trustedProxies = ['10.0.0.1', '10.0.0.2'];
+$app->add(new RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
+*/
